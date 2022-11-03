a21593cd0864282f51da31f645aa9de46f6cf84b
Boris-Maslenov <ya.maslenov@yandex.ru>
2022-11-03 17:55:43 +0300

    delete redux devtools

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,7 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import ReduxThunk from 'redux-thunk';
 import reducer from '../reducer/reducer';
 
-const store = createStore(reducer, compose( applyMiddleware(ReduxThunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() ) );
+//const store = createStore(reducer, compose( applyMiddleware(ReduxThunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() ) );
+const store = createStore(reducer,  applyMiddleware(ReduxThunk) );
 
 export default store;
